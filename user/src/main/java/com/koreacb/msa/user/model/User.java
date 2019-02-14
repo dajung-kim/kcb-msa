042a771b9bc9741b5fb7ce25b4b630979071a1b1
Myungkyo Jung <myungkyo.jung@koreacb.com>
2019-02-14 09:16:22 +0900

    Added REST Repository for User

---
+++ user/src/main/java/com/koreacb/msa/user/model/User.java
@@ -0,0 +1,16 @@
+package com.koreacb.msa.user.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+}
