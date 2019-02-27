cab1807ecff22cf9a5bbc4fb8bb9577b53a5841e
Myungkyo Jung <myungkyo.jung@koreacb.com>
2019-02-27 18:10:32 +0900

    remove unnecessary dependency

---
+++ user/src/main/java/com/koreacb/msa/user/UserApplication.java
@@ -0,0 +1,15 @@
+package com.koreacb.msa.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+}
+
---
+++ user/src/main/java/com/koreacb/msa/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.koreacb.msa.user.repository;
+
+import com.koreacb.msa.user.model.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+
+//public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository  {
+
+}
