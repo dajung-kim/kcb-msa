75f4c3c10eb69a222d67142e886e8604c5355723
Myungkyo Jung <myungkyo.jung@koreacb.com>
2019-03-07 13:22:01 +0900

    #4 Loan API <---> Edge Service 연결

--- edge-service/src/main/java/com/koreacb/msa/edge/client/LoanClient.java
+++ edge-service/src/main/java/com/koreacb/msa/edge/client/LoanClient.java
@@ -1,7 +1,11 @@
 package com.koreacb.msa.edge.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "loan")
 public interface LoanClient {
+    @GetMapping(value = "/loans/{id}")
+    String getLoanInfo(@PathVariable("id") long id);
 }
