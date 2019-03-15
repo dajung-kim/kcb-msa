4e8ee88efbf09fec05bba64155497b178167a068
Myungkyo Jung <myungkyo.jung@koreacb.com>
2019-03-15 16:34:13 +0900

    spring-boot 버전 수정

--- loan/src/main/java/com/koreacb/msa/loan/controller/LoanController.java
+++ loan/src/main/java/com/koreacb/msa/loan/controller/LoanController.java
@@ -1,8 +1,7 @@
 package com.koreacb.msa.loan.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,21 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/loans")
+@Slf4j
 public class LoanController {
-
-    private final Logger logger = LoggerFactory.getLogger(LoanController.class);
 
     @GetMapping("/hello")
     @HystrixCommand(fallbackMethod = "fallback")
     public String loanGreeting(@RequestParam(required = false) final String name) {
+        log.debug("/hello");
         if (name == null) {
-            throw new RuntimeException("Error occurred!");
+            throw new IllegalStateException("Error occurred!");
         }
         return name;
     }
 
     private String fallback(final String name) {
-        logger.debug("Entered fallback()");
+        log.debug("Entered fallback()");
         return "fallback " + name;
     }
 }
