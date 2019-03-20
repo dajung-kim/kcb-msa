f7e6266b3011114a9392b1dbfb146dfdd8487038
Myungkyo Jung <mk.francis.jung@gmail.com>
2019-03-20 17:57:45 +0900

    의존성 최적화 및 Reflection으로 모든 컨트롤러 메소드 조회하는 서비스 개발

--- loan/src/main/java/com/koreacb/msa/loan/controller/LoanController.java
+++ loan/src/main/java/com/koreacb/msa/loan/controller/LoanController.java
@@ -1,25 +1,108 @@
 package com.koreacb.msa.loan.controller;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.koreacb.msa.loan.Description;
+import com.koreacb.msa.loan.model.Loan;
+import com.koreacb.msa.loan.model.Service;
+import com.koreacb.msa.loan.repository.LoanRepository;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.val;
+import org.reflections.Reflections;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/loans")
 @Slf4j
 public class LoanController {
 
+    private final LoanRepository loanRepository;
+
+    @Autowired
+    public LoanController(LoanRepository loanRepository) {
+        this.loanRepository = loanRepository;
+    }
+
     @GetMapping("/hello")
     @HystrixCommand(fallbackMethod = "fallback")
+    @Description("이름을 입력받아서 화면에 출력해 주는 API")
     public String loanGreeting(@RequestParam(required = false) final String name) {
         log.debug("/hello");
         if (name == null) {
             throw new IllegalStateException("Error occurred!");
         }
         return name;
+    }
+
+    @GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Resources<Resource> getProfile(@RequestParam(required = false) final String name) {
+        log.debug("Start HATEOAS profile");
+        val all = loanRepository.findAll();
+        val list = new ArrayList<Resource>();
+        for (Loan loan : all) {
+            val resource = new Resource<>(loan);
+            resource.add(linkTo(methodOn(this.getClass()).getProfile(name)).withSelfRel());
+            list.add(resource);
+        }
+        return new Resources<>(list);
+    }
+
+    @Description("테스트용 더미 메소드")
+    public String dummyMethod(@RequestParam("name") final String name, @RequestParam("age") final int age, final Map<String, String> map) {
+        return "";
+    }
+
+    @GetMapping("/apis")
+    @Description("API 목록 조회")
+    public String getApiInformation() {
+        val reflections = new Reflections("com.koreacb.msa");
+        val controllers = reflections.getTypesAnnotatedWith(RestController.class);
+
+        val services = new ArrayList<>();
+        for (Class clazz : controllers) {
+            val declaredMethods = clazz.getDeclaredMethods();
+            for (Method method : declaredMethods) {
+                val service = new Service(method);
+                services.add(service);
+                log.debug("{}", service);
+            }
+        }
+        val listType = new TypeToken<List<Service>>() {
+        }.getType();
+
+        return new GsonBuilder().setPrettyPrinting().create().toJson(services, listType);
+    }
+
+    @GetMapping("/apis/{name}")
+    @Description("API 검색")
+    @HystrixCommand(fallbackMethod = "fallback")
+    public String getApiInformation(@PathVariable("name") final String name) {
+        val reflections = new Reflections("com.koreacb.msa");
+        val controllers = reflections.getTypesAnnotatedWith(RestController.class);
+
+        for (Class clazz : controllers) {
+            val declaredMethods = clazz.getDeclaredMethods();
+            for (Method method : declaredMethods) {
+                if (method.getName().equals(name)) {
+                    val service = new Service(method);
+                    return new GsonBuilder().setPrettyPrinting().create().toJson(service);
+                }
+            }
+        }
+        return "";
     }
 
     private String fallback(final String name) {
---
+++ loan/src/main/java/com/koreacb/msa/loan/model/Service.java
@@ -0,0 +1,42 @@
+package com.koreacb.msa.loan.model;
+
+import com.koreacb.msa.loan.Description;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.val;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Service {
+    private List<String> input = new ArrayList<>();
+    private String output;
+    private String name;
+    private String description;
+
+    public Service(Method method) {
+        val parameters = method.getParameters();
+        for (Parameter parameter : parameters) {
+            RequestParam requestParam = parameter.getAnnotation(RequestParam.class);
+            if (requestParam != null) {
+                this.input.add(requestParam.value() + "|" + parameter.getType().getTypeName());
+            } else {
+                PathVariable pathVariable = parameter.getAnnotation(PathVariable.class);
+                if (pathVariable != null) {
+                    this.input.add(pathVariable.value() + "|" + parameter.getType().getTypeName());
+                }
+            }
+        }
+        this.output = method.getReturnType().getTypeName();
+        this.description = method.getAnnotation(Description.class) != null ? method.getAnnotation(Description.class).value() : "";
+        this.name = method.getName();
+    }
+}
