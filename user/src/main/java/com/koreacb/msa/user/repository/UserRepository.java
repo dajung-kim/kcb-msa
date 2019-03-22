b09f04d3a68f3fd851f831583bdb209a8fc6889f
JungYoungsu <terun707@naver.com>
2019-03-22 14:24:57 +0900

    #3 User API 반영. (graphql, graphiql)
    /graphql URL로 오픈 됨. /grapqhiql 에서 테스트 가능

--- user/src/main/java/com/koreacb/msa/user/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.koreacb.msa.user.repository;
-
-import com.koreacb.msa.user.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
