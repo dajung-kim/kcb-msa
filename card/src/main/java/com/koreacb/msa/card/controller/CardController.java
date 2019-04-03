04eb26c40e857246ad188c840e55523a2149f729
dajung <dajung@koreacb.com>
2019-04-03 16:25:49 +0900

    #10 spring cloud config 서버 구성
    - config server 구성
    - Card 서비스에 대해 config client 구성

--- card/src/main/java/com/koreacb/msa/card/controller/CardController.java
+++ card/src/main/java/com/koreacb/msa/card/controller/CardController.java
@@ -5,18 +5,27 @@ import com.koreacb.msa.card.core.ResultGenerator;
 import com.koreacb.msa.card.model.Card;
 import com.koreacb.msa.card.service.impl.CardServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
+@RefreshScope
 public class CardController {
 
     @Autowired
     private CardServiceImpl cardServiceImpl;
+    @Value("${kcb.config.env}")
+    private String env;
+    @Value("${kcb.config.module}")
+    private String module;
 
     @GetMapping("/cards")
     public List<Card> getCardsByTxAgncCd(@RequestParam("type") String txAgncCd) {
@@ -33,5 +42,12 @@ public class CardController {
         return ResultGenerator.genSuccessResult(cardServiceImpl.findByTxAgncCd(txAgncCd)).toString();
     }
 
+    @GetMapping("/config")
+    public Map<String, String> getConfig(){
+        Map<String, String> keyword = new HashMap<>();
+        keyword.put("env",env);
+        keyword.put("module",module);
+        return keyword;
+    }
 
 }
