bfda2f26e85942f526ac5fdbc05bb4aec5bc72dd
Myungkyo Jung <mk.francis.jung@gmail.com>
2019-04-03 15:24:50 +0900

    여러 stream 동시에 받아서 처리하기

--- gateway/src/main/java/com/koreacb/msa/gateway/controller/GatewayController.java
+++ gateway/src/main/java/com/koreacb/msa/gateway/controller/GatewayController.java
@@ -1,7 +1,8 @@
 package com.koreacb.msa.gateway.controller;
 
-import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @RestController
 @RequestMapping("/gw")
@@ -31,7 +33,7 @@ public class GatewayController {
     @GetMapping("/monoString")
     public Mono<String> monoString(@RequestParam final MultiValueMap<String, String> map) {
         log.debug("Entered /monoString");
-        Mono<String> stringMono = webClient.get().uri(uriBuilder -> uriBuilder.path("/reactor/monoString").queryParams(map).build()).retrieve().bodyToMono(String.class);
+        Mono<String> stringMono = webClient.get().uri(uriBuilder -> uriBuilder.path("/apis").queryParams(map).build()).retrieve().bodyToMono(String.class);
         String block = stringMono.block();
         log.debug("Result: {}", stringMono);
         stringMono.subscribe(System.out::println);
@@ -42,18 +44,40 @@ public class GatewayController {
     @GetMapping("/monoString2")
     public String monoString2() {
         log.debug("Entered /monoString2");
-        Mono<String> mono1 = webClient.get().uri(uriBuilder -> uriBuilder.path("/reactor/monoString").queryParam("id", 1).build()).retrieve().bodyToMono(String.class);
-        Mono<String> mono2 = webClient.get().uri(uriBuilder -> uriBuilder.path("/reactor/monoString").queryParam("id", 2).build()).retrieve().bodyToMono(String.class);
-        Mono<String> mono3 = webClient.get().uri(uriBuilder -> uriBuilder.path("/reactor/monoString").queryParam("id", 3).build()).retrieve().bodyToMono(String.class);
-//        Mono<Void> when = Mono.when(mono1, mono2, mono3);
-//        when.block();
+        val serviceList = retrieveServiceList();
+        List<Mono<String>> producers = new ArrayList<>();
+        val result = new HashMap<String, String>();
+
+        for (final Service each : serviceList) {
+            Mono<String> mono = webClient.get().uri(uriBuilder -> uriBuilder.path(each.endpointUrl).queryParam("id", each.param).build()).retrieve().bodyToMono(String.class);
+            producers.add(mono);
+            mono.subscribe(response -> result.put(each.endpointUrl, response));
+        }
+//        Mono<String> mono1 = webClient.get().uri(uriBuilder -> uriBuilder.path("/reactor/monoString").queryParam("id", 1).build()).retrieve().bodyToMono(String.class);
+//        System.out.println("1");
+//        Mono<String> mono2 = webClient.get().uri(uriBuilder -> uriBuilder.path("/reactor/monoString").queryParam("id", 2).build()).retrieve().bodyToMono(String.class);
+//        System.out.println("2");
+//        Mono<String> mono3 = webClient.get().uri(uriBuilder -> uriBuilder.path("/reactor/monoString").queryParam("id", 3).build()).retrieve().bodyToMono(String.class);
+//        System.out.println("3");
 
 
-        mono1.subscribe(System.out::println);
-        mono2.subscribe(System.out::println);
-        mono3.subscribe(System.out::println);
-
-
+        Mono.when(producers).block();
+        log.debug("Result: " + result.toString());
         return null;
+    }
+
+    private List<Service> retrieveServiceList() {
+        List<Service> services = new ArrayList<>();
+        services.add(new Service("/reactor/monoString", "1"));
+        services.add(new Service("/reactor/monoString", "2"));
+        services.add(new Service("/reactor/monoString", "3"));
+
+        return services;
+    }
+
+    @AllArgsConstructor
+    private class Service {
+        String endpointUrl;
+        String param;
     }
 }
